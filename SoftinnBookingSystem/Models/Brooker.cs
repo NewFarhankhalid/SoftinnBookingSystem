8d3137db1a98e932f442f34d0662877f1b6ed26d
Abbas <abbasbukhari698@gmail.com>
2024-04-20 15:52:50 +0500

    Abbas Commit

---
+++ SoftinnBookingSystem/Models/Brooker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftinnBookingSystem.Models
+{
+    public class Brooker
+    {
+        public int BrookerID { get; set; }
+        public string BrookerMC  { get; set; }
+        public string BrookerUsDot { get;}
+        public string BrookerBusinessName { get; set; }
+        public string BrookerEmail { get;set; }
+        public string BrookerPhone { get;}
+        public string BrookerFax { get;}
+        public string BrookerAddress { get;}
+    }
+}
